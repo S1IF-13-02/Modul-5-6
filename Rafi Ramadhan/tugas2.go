c54ce702763ddd77a1104713dc51857b0adc19cf
rafiramadhan <rafirama1020@gmail.com>
2025-10-14 19:51:20 +0700

    Create tugas2.go

---
+++ Rafi Ramadhan/tugas2.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+    "fmt"
+    "math"
+)
+
+func main() {
+    var n int
+	 var r, t float64
+       
+    fmt.Print("Masukkan jumlah kerucut: ")
+    fmt.Scan(&n)
+
+    for i := 1; i <= n; i++ {
+        fmt.Print("Masukkan jari-jari dan tinggi kerucut ke-", i, ": ")
+        fmt.Scan(&r, &t)
+
+        volume := (1.0 / 3.0) * math.Pi * r * r * t
+        fmt.Println(volume)
+    }
+}
