29d621ae5f0e471dc2e72ecbda7918d935459c99
rafiramadhan <rafirama1020@gmail.com>
2025-10-14 19:53:02 +0700

    Update tugas4.go

--- Rafi Ramadhan/tugas4.go
+++ Rafi Ramadhan/tugas4.go
@@ -1,1 +1,16 @@
+package main
 
+import "fmt"
+
+func main() {
+    var n int
+    fmt.Print("Masukkan bilangan: ")
+    fmt.Scan(&n)
+
+    var hasil int = 1
+    for i := 1; i <= n; i++ {
+        hasil = hasil * i
+    }
+
+    fmt.Println("Hasil faktorial dari", n, "adalah:", hasil)
+}
