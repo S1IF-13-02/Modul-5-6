ba78c5370ea271e20e5f70989ed47a285bff715b
alvin09-a11y <celinecristina838@gmail.com>
2025-10-18 11:30:07 +0700

    Add power calculation program in no3.go

---
+++ alvin setya wardana/no3.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+func main() {
+	var bp, bk int
+	hasil := 1
+	fmt.Print("masukkan angka : ")
+	fmt.Scan(&bp)
+	fmt.Print("masukkan pangkat : ")
+	fmt.Scan(&bk)
+	for i := 1; i <= bk; i++ {
+		hasil = hasil * bp
+	}
+	fmt.Println(hasil)
+}
