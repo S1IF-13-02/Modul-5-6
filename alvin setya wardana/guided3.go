95667367aaaf13cad80ba811b00a4c1516038f5e
alvin09-a11y <celinecristina838@gmail.com>
2025-10-19 18:18:20 +0700

    Update guided3.go

--- alvin setya wardana/guided3.go
+++ alvin setya wardana/guided3.go
@@ -2,15 +2,14 @@ package main
 
 import "fmt"
 
-func main(){
-	var n int
+func main() {
+	var j, v1, v2 int
 	var hasil int
-
-	fmt.Print("Masukan bilangan: ")
-	fmt.Scan(&n)
-
-	for i := 1; i <= n; i++ {
-		hasil = hasil + i
+	fmt.Scan(&v1, &v2)
+	hasil = 0
+	for j = 1; j <= v2; j += 1 {
+		hasil = hasil + v1
 	}
 	fmt.Println(hasil)
 }
+
